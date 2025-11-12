70dc7249731b9c24bc7f2a6cdfd7773fdd104148
Karim Anaya-Izquierdo <kai21@vpx2l6g20h.campus.bath.ac.uk>
2025-11-12 11:15:10 +0000

    feat: Apply JetBrains Mono font to code text

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,10 @@
 import type { Metadata } from "next";
-import { JetBrains_Mono, Geist_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
 
 const jetBrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -25,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${jetBrainsMono.variable} ${geistMono.variable}`}>
+      <body className={`${jetBrainsMono.variable}`}>
         {children}
       </body>
     </html>
